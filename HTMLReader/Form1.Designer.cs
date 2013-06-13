4be4fcb98ba0c885e45102aaa17dcf4701874a9d
tranhaidang511 <tranhaidang511@gmail.com>
2013-06-13 23:32:42 +0900

    Read HTML by CPP

---
+++ HTMLReader/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace HTMLReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.URL = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(417, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // URL
+            // 
+            this.URL.AutoSize = true;
+            this.URL.Location = new System.Drawing.Point(27, 31);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(29, 13);
+            this.URL.TabIndex = 1;
+            this.URL.Text = "URL";
+            this.URL.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(518, 135);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.URL);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Input URL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label URL;
+        private System.Windows.Forms.Button button1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HTMLReader/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using WinSCP;
+
+namespace HTMLReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = null;
+            string url = textBox1.Text;
+            string filename = null;
+            number++;
+            WebResponse response = null;
+            StreamReader wr = null, fr = null;
+            StreamWriter writer = null;
+            HttpWebRequest request1;
+            FtpWebRequest request2;
+
+            try
+            {
+                //Read template until <div class="noPc">
+                fr = new StreamReader("column0001.html");
+                filename = "Number" + DateTime.Now.ToString("yyyyMMddhhmm") + number.ToString("D2") + ".html";
+                writer = new StreamWriter(filename);
+                label1.Text = "Writing to file...";
+                while ((result = fr.ReadLine()) != "<a href=\"https://tv.pacificleague.jp/ptv/api/copyUrl/play?movieId=YuKvI3cdyHI\"><img src=\"img/img_movie.jpg\" alt=\"\" title=\"\" /></a>")
+                {
+                    writer.WriteLine(result);
+                }
+                writer.WriteLine("<a href=\"" + textBox1.Text + "\"><img src=\"img/img_movie.jpg\" alt=\"\" title=\"\" /></a>");
+/*
+                //Read HTML page for Android  
+                request1 = (HttpWebRequest)WebRequest.Create(url);
+                request1.Method = "GET";
+                request1.UserAgent = "Mozilla/5.0 (Android 4.0.3)";
+                response = request1.GetResponse();
+                wr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                result = wr.ReadToEnd();
+                writer.WriteLine(result);
+                wr.Close();
+                response.Close();
+*/
+                //Continue reading template until "<div class="noSp">
+                while ((result = fr.ReadLine()) != "<iframe src=\"https://tv.pacificleague.jp/ptv/api/copyUrl/play?movieId=YuKvI3cdyHI\" frameborder=\"0\" scrolling=\"no\" width=\"680\" height=\"460\"></iframe>")
+                {
+                    writer.WriteLine(result);
+                }                
+                writer.WriteLine("<iframe src=\"" + textBox1.Text + "\" frameborder=\"0\" scrolling=\"no\" width=\"680\" height=\"460\"></iframe>");
+/*
+                //Read HTML page for Windows
+                request1 = (HttpWebRequest)WebRequest.Create(url);
+                request1.Method = "GET";
+                request1.UserAgent = "Mozilla/5.0 (Windows NT 6.1)";
+                response = request1.GetResponse();
+                wr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                result = wr.ReadToEnd();
+                writer.WriteLine(result);
+                wr.Close();
+                response.Close();
+*/
+                ////Read remaining part of template
+                result = fr.ReadToEnd();
+                writer.WriteLine(result);
+                fr.Close();
+                writer.Close();
+                label1.Text = "Complete writing";
+
+                //Upload file with FTP
+/*                label1.Text = "Uploading file...";
+                request2 = (FtpWebRequest)WebRequest.Create("ftp://"+number+".html");//Fill in FTP address
+                request2.Method = WebRequestMethods.Ftp.UploadFile;
+                request2.Credentials = new NetworkCredential("username", "password");//Change username and password
+                fr = new StreamReader(number + ".html");
+                byte[] fileContents = Encoding.UTF8.GetBytes(fr.ReadToEnd());
+                request2.ContentLength = fileContents.Length;
+                Stream requestStream = request2.GetRequestStream();
+                requestStream.Write(fileContents, 0, fileContents.Length);
+                FtpWebResponse response2 = (FtpWebResponse)request2.GetResponse();
+                label1.Text = "Success !";
+                requestStream.Close();
+                response2.Close();*/
+
+                //Upload file with SSH
+                SessionOptions sessionOptions = new SessionOptions
+                {//Fill in HostName, PortNumber(if necessary), UserName, Password, SshHostKeyFingerprint
+                    Protocol = Protocol.Scp,
+                    HostName = "210.129.194.50",
+                    PortNumber = 22605,
+                    UserName = "spo01",
+//                    Password = "",
+                    SshHostKeyFingerprint = "ssh-rsa 2048 32:a4:7f:c3:2e:2c:a2:0a:1d:8e:41:cc:92:46:37:f5",
+                    SshPrivateKeyPath = @"C:\Users\HaiDang\Documents\Visual Studio 2012\Projects\HTMLReader\HTMLReader\bin\Debug\id_rsa-spo01.ppk",
+                };
+                label1.Text = "Uploading file...";
+                using (Session session = new Session())
+                {
+                    session.Open(sessionOptions);
+//                    session.ExecutablePath = ".";
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+                    TransferOperationResult transferResult;
+                    transferResult = session.PutFiles(filename, "/home/spo01/", false, transferOptions);
+                    transferResult.Check();
+                    label1.Text = "Success !";
+                }
+            }
+            catch (Exception ex)
+            {
+                // handle error
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public int number { get; set; }//file name
+
+        private void eventLog1_EntryWritten(object sender, System.Diagnostics.EntryWrittenEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
